f4ee14215f9d1712f49629e99693c72ba4eb6f20
RaineShadow <twitch.raineshadow@gmail.com>
2021-12-06 16:24:41 -0600

    Reformated.

--- src/Day1.java
+++ src/Day1.java
@@ -3,35 +3,61 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Day1 {
-	public static void main(String[] args) {
-		int numfirst = 0, numlast = 0, inctotal = 0;
+	public static int readLine(int first, int last) {
+		int inctotal = 0;
 		
-		//Read in each line of the txt file
 		try(BufferedReader br = new BufferedReader(new FileReader("src/Day1_input.txt"))){
 			String line = br.readLine();
 			while(line != null) {
-				//Convert text to int
-				numfirst = Integer.parseInt(line);
+				first = Integer.parseInt(line);
 				//System.out.println("first: " + line);
 				line = br.readLine();
 				//System.out.println("second: " + line);
 				if(line != null) {
-					numlast = Integer.parseInt(line);
+					last = Integer.parseInt(line);
 				}
 				
-				//Figure out if second line int is greater than first line int
-				if(numlast > numfirst) {
-					//Add to number of times increased
+				boolean inc = compareLines(first, last);
+				if(inc == true) {
 					inctotal++;
 				}
 			}
-			
-			br.close();
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
 		
-		//Output number once reached end of file
+		return inctotal;
+	}
+	
+	public static boolean compareLines(int first, int last) {
+		boolean inc = false;
+		
+		if(last > first) {
+			inc = true;
+		}
+		
+		return inc;
+	}
+	
+	public static void Day1Part1() {
+		int numfirst = 0, numlast = 0, inctotal = 0;
+		
+		inctotal = readLine(numfirst, numlast);
+
 		System.out.println(inctotal);
+	}
+	
+	public static void Day1Part2() {
+		int sumfirst = 0, sumlast = 0, inctotal = 0;
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Day 1: Part 1");
+		Day1Part1();
+		
+		System.out.println("Day 1: Part 2");
+		Day1Part2();
 	}
 }
